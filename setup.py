e0a08481e0f7ba1284e5456d1f3973e119fd3aa6
Stephan Hoyer <shoyer@climate.com>
2015-04-07 16:12:19 -0700

    pypi classifiers

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
+import os
 from setuptools import setup, find_packages
 
 
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Topic :: Scientific/Engineering',
+]
+
+
 setup(name='h5netcdf',
-      version='0.1.dev0',
-      license='MIT',
+      description='netCDF4 via h5py',
+      long_description=(open('README.rst').read()
+                        if os.path.exists('README.rst')
+                        else ''),
+      version='0.1',
+      license='BSD',
+      classifiers=CLASSIFIERS,
       author='Stephan Hoyer',
       author_email='shoyer@gmail.com',
+      url='https://github.com/shoyer/h5netcdf',
       install_requires=['h5py'],
+      tests_require=['netCDF4', 'pytest'],
       packages=find_packages())
