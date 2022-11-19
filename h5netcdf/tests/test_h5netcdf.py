eb5e6c4dd342ffda993e29a583ab7d9eb75bdbf1
Mark Harfouche <mark.harfouche@gmail.com>
2022-11-19 18:45:47 -0500

    more revert.

--- h5netcdf/tests/test_h5netcdf.py
+++ h5netcdf/tests/test_h5netcdf.py
@@ -1638,6 +1638,18 @@ def test_track_order_specification(tmp_local_netcdf):
 # This should always work with the default file opening settings
 # https://github.com/h5netcdf/h5netcdf/issues/136#issuecomment-1017457067
 def test_more_than_7_attr_creation(tmp_local_netcdf):
+   with h5netcdf.File(tmp_local_netcdf, "w") as h5file:
+        for i in range(100):
+            h5file.attrs[f"key{i}"] = i
+            h5file.attrs[f"key{i}"] = 0
+
+
+# Add a test that is supposed to fail in relation to issue #136
+# We choose to monitor when h5py will have fixed their issue in our test suite
+# to enhance maintainability
+# https://github.com/h5netcdf/h5netcdf/issues/136#issuecomment-1017457067
+@pytest.mark.parametrize("track_order", [False, True])
+def test_more_than_7_attr_creation_track_order(tmp_local_netcdf, track_order):
     h5py_version = version.parse(h5py.__version__)
     if track_order and h5py_version < version.parse("3.7.0"):
         expected_errors = pytest.raises(KeyError)
